54826b2440cb4d1198f49bba47d9572d949d689f
Federico Bricker <fbricker@gmail.com>
2014-05-27 15:06:30 -0300

    Added Control to avoid too many ads to be displayed

--- extension/fgl/FGL.hx
+++ extension/fgl/FGL.hx
@@ -2,10 +2,29 @@ package extension.fgl;
 
 class FGL {
 	////////////////////////////////////////////////////////////////////////////	
-	public static var showAd:Void->Void = 
+	private static var __showAd:Void->Void = 
 		#if android
 			openfl.utils.JNI.createStaticMethod("lime/extension/fgl/FGL", "showAd", "()V");
 		#else
 			function(){};
-		#end	
+		#end
+
+	////////////////////////////////////////////////////////////////////////////	
+	private static var lastTiempoIntersital:Int = -60*1000;
+	private static var displayCallsCounter:Int = 0;
+
+	public static function showAd(minInterval:Int=60, minCallsBeforeDisplay:Int=0) {
+		#if android
+			displayCallsCounter++;
+			if( (flash.Lib.getTimer()-lastTiempoIntersital)<(minInterval*1000) ) return;
+			if( minCallsBeforeDisplay > displayCallsCounter ) return;
+			displayCallsCounter = 0;
+			lastTiempoIntersital = flash.Lib.getTimer();
+			try{
+				__showAd();
+			}catch(e:Dynamic){
+				trace("ShowInterstitial Exception: "+e);
+			}
+		#end
+	}
 }
