01b2c29a65de2459786965a30513631c63e3ca1c
Federico Bricker <fbricker@gmail.com>
2014-06-06 21:48:10 -0300

    Added OpenFL Tracking code

--- dependencies/fgl/src/lime/extension/fgl/FGL.java
+++ dependencies/fgl/src/lime/extension/fgl/FGL.java
@@ -6,7 +6,7 @@ import org.haxe.extension.Extension;
 public class FGL extends Extension {
     
 	public static void showAd() {
-		mainActivity.sendBroadcast(new Intent("com.fgl.INVOKE").putExtra("command", "showAd"));
+		mainActivity.sendBroadcast(new Intent("com.fgl.INVOKE").putExtra("command", "showAd").putExtra("source", "openfl"));
 	}
 
 }
